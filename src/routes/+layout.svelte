80184fe954ac2de4eda40ab9643cf349202f0da8
haparece-fc <jougan.rennisharingan@gmail.com>
2025-10-21 14:43:29 +0800

    Creatation of the login page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,17 @@
 	let { children } = $props();
 	import Header from '$lib/components/layout/Header.svelte';
 	import '../app.css';
+	import { page } from '$app/state';
+
+	const route = $derived(page.route.id as string);
 </script>
 
 <div class="flex min-h-screen flex-col">
-	<Header />
+	{#if route && !route.startsWith('/auth')}
+		<Header />
+	{/if}
 	<div class="h-[calc(100vh_-_64px)] bg-slate-50">
-		<main class="mx-auto w-full max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+		<main class="mx-auto h-full w-full max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
 			{@render children()}
 		</main>
 	</div>
