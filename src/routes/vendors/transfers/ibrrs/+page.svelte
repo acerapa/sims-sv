f740672b8d72b6917ee1aa4b45282a9a0590457b
haparece-fc <jougan.rennisharingan@gmail.com>
2025-12-01 12:18:21 +0800

    transfer page setup

---
+++ src/routes/vendors/transfers/ibrrs/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Card, CardDescription, CardHeader, CardTitle } from '$lib/components/ui/card';
+</script>
+
+<Card>
+	<CardHeader>
+		<div class="flex items-center justify-between">
+			<div class="space-y-2">
+				<CardTitle>Inter-Branch Receiving Reports</CardTitle>
+				<CardDescription>Track and manage inventory receipts from other locations</CardDescription>
+			</div>
+		</div>
+	</CardHeader>
+</Card>
---
+++ src/routes/vendors/transfers/strs/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import STRForm from '$lib/components/pages/vendors/transfers/STRForm.svelte';
+	import { Card, CardDescription, CardHeader, CardTitle } from '$lib/components/ui/card';
+</script>
+
+<Card>
+	<CardHeader>
+		<div class="flex items-center justify-between">
+			<div class="space-y-2">
+				<CardTitle>Stock Transfer Requests</CardTitle>
+				<CardDescription>Track and manage inventory transfers between locations</CardDescription>
+			</div>
+			<STRForm />
+		</div>
+	</CardHeader>
+</Card>
